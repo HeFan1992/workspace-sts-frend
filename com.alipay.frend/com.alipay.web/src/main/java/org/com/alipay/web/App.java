1a81e3374088dca8a04da718a1ef75b9de83068f
HeFan1992 <1368898852@qq.com>
2018-07-08 04:00:20 +0800

    提交项目

---
+++ com.alipay.frend/com.alipay.web/src/main/java/org/com/alipay/web/App.java
@@ -0,0 +1,13 @@
+package org.com.alipay.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
